9e3333ffac9a7bfc84b62aee7e0d4cbe8dea432d
Oyomi <seayomi@gmail.com>
2020-03-02 16:15:56 +0100

    app setup

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
